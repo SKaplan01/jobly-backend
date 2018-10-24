102382d84957efe35f68a4534ff4123212849d2a
Sarah Kaplan <skaplan520@gmail.com>
2018-10-24 10:40:29 -0700

    added unit test for partialUpdate function

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,17 @@
-describe("partialUpdate()", () => {
-  it("should generate a proper partial update query with just 1 field",
-      function () {
+process.env.NODE_ENV = 'test';
+const sqlForPartialUpdate = require('../../helpers/partialUpdate');
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
-
+describe('partialUpdate()', function() {
+  it('should generate a proper partial update query with just 1 field', async function() {
+    const query = sqlForPartialUpdate(
+      'companies',
+      { num_employees: 3000 },
+      'handle',
+      'amz'
+    );
+    expect(query.query).toBe(
+      'UPDATE companies SET num_employees=$1 WHERE handle=$2 RETURNING *'
+    );
+    expect(query.values).toEqual([3000, 'amz']);
   });
 });
---
+++ data.sql
@@ -0,0 +1,9 @@
+
+-- schema for companies table
+
+CREATE TABLE companies(
+handle TEXT PRIMARY KEY,
+name TEXT NOT NULL,
+num_employees INTEGER,
+description TEXT,
+logo_url TEXT);
